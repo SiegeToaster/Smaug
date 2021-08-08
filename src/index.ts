8617c02219f7ed657b91b5ed54b95497fa684291
Siege <siegetoaster@gmail.com>
2021-08-08 13:16:56 -0700

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv'
+import { Client } from 'eris'
+config()
+const bot = new Client(process.env.TOKEN)
+
+bot.once('ready', () => {
+    console.log(`Logged in as ${bot.user.username}`)
+});
+bot.on('messageCreate', (message) => {
+    if (message.content.toLocaleLowerCase() === "?ping") {
+        bot.createMessage(message.channel.id, "pong");
+    }
+})
+
+bot.connect()
