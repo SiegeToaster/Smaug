ea079544ec23549e1ff2791095f8578ce22e9ce6
SiegeToaster <81945479+SiegeToaster@users.noreply.github.com>
2021-10-05 10:48:18 -0700

    remove index comment

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-/* import { Client, Intents } from "discord.js"
+import { Client, Intents } from "discord.js"
 import { config } from 'dotenv'
 config()
 
@@ -31,21 +31,3 @@ client.on('interactionCreate', async interaction => {
 	}
 	}
 })
- */
-
-import { SlashCommandBuilder } from '@discordjs/builders'
-
-export const data = new SlashCommandBuilder()
-	.setName('test')
-	.setDescription('testDescription')
-	.addStringOption(option => {
-		return option.setName('testoption1')
-			.setDescription('testOption1 description')
-			.setRequired(false)
-			.addChoices([['testChoice1', 'whatever1'], ['testChoice2', 'whatever2']])
-	})
-
-console.log(data.toJSON())
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-console.log(data.toJSON().options[0].choices)
