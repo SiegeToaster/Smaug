151eae796e0df2580e0adfc26312f314b7aedf54
Siege <81945479+SiegeToaster@users.noreply.github.com>
2021-10-28 21:21:23 -0700

    joinVoice working, plays song :)

--- src/commands/general/joinVoice.ts
+++ src/commands/general/joinVoice.ts
@@ -1,10 +1,9 @@
 import {
 	CommandInteraction,
 	VoiceChannel,
-	StageChannel,
 } from 'discord.js'
 import { SlashCommandBuilder } from '@discordjs/builders'
-import {createAudioPlayer} from '@discordjs/voice'
+import { createAudioPlayer } from '@discordjs/voice'
 import { musicFunctions } from './../../functions/functionExports'
 // https://github.com/discordjs/voice/blob/main/examples/basic/
 
@@ -14,27 +13,19 @@ export const commandData = new SlashCommandBuilder()
 	.setName('joinvoice')
 	.setDescription('Join voice channel.')
 
-export default function joinVoice(interaction: CommandInteraction): void {
+export default async function joinVoice(interaction: CommandInteraction): Promise<void> {
 	const player = createAudioPlayer()
 	// @ts-expect-error memeber will (hopefully) never be type APIInteractionGuildMember
-	const channel: VoiceChannel | StageChannel | null | undefined = interaction.member?.voice.channel
-	if (!channel) return console.error('joinVoice error: channel is null or undefined:\n', channel)
-}
+	const channel: VoiceChannel | null | undefined = interaction.member?.voice.channel
+	if (!channel) return interaction.reply('No voice channel detected.  Join a voice channel and try again.')
 
-async function yaboi(message) {
-	if (message.content === '-join') {
-		const channel = message.member?.voice.channel
-
-		if (channel) {
-			try {
-				const connection = await musicFunctions.connectToChannel(channel)
-				connection.subscribe(player)
-				message.reply('Playing now!')
-			} catch (error) {
-				console.error(error)
-			}
-		} else {
-			message.reply('Join a voice channel then try again!')
-		}
+	try {
+		const connection = await musicFunctions.connectToChannel(channel)
+		connection.subscribe(player)
+		interaction.reply('Playing.')
+	} catch (err) {
+		console.error('joinVoice connection error: ', err)
 	}
-}
+
+	musicFunctions.playSong(player)
+} // ToDo: play songs other than the techno thing
