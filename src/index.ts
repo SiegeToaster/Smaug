a1bac2a93cf0d85a9ab1c4ab9ea9399992845800
Siege <81945479+SiegeToaster@users.noreply.github.com>
2021-12-28 21:12:01 -0800

    song streaming testing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Client } from "discord.js"
 import { AudioPlayer } from "@discordjs/voice"
 import { config } from 'dotenv'
+import https from 'https'
 config()
 
 const client = new Client({ 
@@ -21,6 +22,34 @@ import catjam from './commands/general/catjam'
 import jamtime from './commands/music/jamtime'
 import joinVoice from './commands/music/joinVoice'
 import play from './commands/music/play'
+
+const testOptions = {
+	hostname: 'api-v2.soundcloud.com',
+	path: '/search/queries',
+	method: 'GET',
+	headers: {
+		q: 'sabotage besatie boys',
+		client_id: 'nzlp05ChzxSyVpcOCKvTIZdwDLZfWM0z',
+		limit: '1',
+		offset: '0',
+		linked_partitioning: '1',
+		app_version: '1638455582',
+		app_locale: 'en',
+	},
+}
+console.log(testOptions)
+const request = https.request(testOptions, res => {
+	console.log(`statusCode: ${res.statusCode}`)
+
+	res.on('data', d=> {
+		process.stdout.write(d)
+	})
+})
+request.on('error', error => {
+	console.error(error)
+})
+
+request.end()
 
 client.login(process.env.DISCORD_TOKEN)
 
